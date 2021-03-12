ef2c62ea7af20d7f741f26a274711d7115f5d059
Tyler Durden <tylerdurden@pop-os.localdomain>
2021-03-12 16:15:34 +0000

    updated respect timelag

--- 13_f_scraper.py
+++ 13_f_scraper.py
@@ -1,6 +1,7 @@
 """ SEC scraping for 13-f Fillings """
 import logging
 import scrapy
+import re
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -8,25 +9,31 @@ class SecSpider(scrapy.Spider):
     """ Spider collects links to sec site and downloads and parses contents of 13-f fillings """
     name = '13f_spider'
 
-    iftt_file = pd.read_csv('SEC13_fFilings_1.csv', header=None)
-    sample = iftt_file.head()
-    # links = iftt_file[iftt_file.columns[2]]
-    links = sample[sample.columns[2]]
-    start_urls = links.tolist()
+    custom_settings = {
+        'AUTOTHROTTLE_ENABLED': True,
+    }
+
+    def start_requests(self):
+        iftt_file = pd.read_csv('SEC13_fFilings_4.csv', header=None)
+        for index, row in iftt_file.iterrows():
+            print(f'Row number: {index}')
+            yield scrapy.Request(row[2], meta={'filer':row[1], 'date_string': row[0]})
 
     def parse(self, response):
         """ Parse filling page and download info table """
         logging.info(response.xpath("//*[contains(text(), 'INFORMATION TABLE')]/preceding-sibling::td/a[contains(text(), '.html')]/@href"))
         info_tables = response.xpath("//*[contains(text(), 'INFORMATION TABLE')]/preceding-sibling::td/a[contains(text(), '.html')]/@href").extract()
+        filer = re.sub(r'[^\sa-zA-Z0-9]', '', response.meta.get('filer')).lower().strip().replace(" ", "_")
+        date_string = response.meta.get('date_string')
         for i, link in enumerate(info_tables):
             print(link)
             print(i)
             absolute_url = response.urljoin(link)
-            yield scrapy.Request(absolute_url, callback=self.parse_info_table, meta={row_index: i})
+            yield scrapy.Request(absolute_url, callback=self.parse_info_table, meta={'file_name': f'{filer}_{i}', 'date_string': date_string})
 
     def parse_info_table(self, response):
         """ Parse info table page and convert to pandas df """
-        index_val = response.meta.get('row_index')
+        file_name = response.meta.get('file_name')
         soup = BeautifulSoup(response.text, 'lxml')
         rows = soup.find_all('tr')[11:]
         positions = []
@@ -41,7 +48,8 @@ class SecSpider(scrapy.Spider):
             dic["SHARE TYPE"] = position[5].text
             dic["PUT_CALL"]=position[6].text
             dic["INVESTMENT_DISCRETION"] = position[7].text
+            dic["DATE_STRING"] = response.meta.get('date_string')
             positions.append(dic)           
         df = pd.DataFrame(positions)
         print(df)
-        df.to_csv(f'{index_val}_result.csv')
+        df.to_csv(f'output_results/file_4/{file_name}_result.csv')
